f126451fbda3c0a5dfa9b3ceac5017ca4af47f62
liujie <liujie045.china@outlook.com>
2019-06-03 19:22:30 +0800

    Handy Commit

---
+++ app/src/main/java/com/handy/basic/app/LocalApplication.java
@@ -0,0 +1,12 @@
+package com.handy.basic.app;
+
+/**
+ * FileName
+ *
+ * @author LiuJie https://github.com/Handy045
+ * @description File Description
+ * @date Created in 2019-06-03 19:22
+ * @modified By liujie
+ */
+public class LocalApplication extends BaseApplication {
+}
